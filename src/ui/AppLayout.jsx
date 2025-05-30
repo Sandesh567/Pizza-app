73eec622d6b480661da705b90ad51a5c5edbd714
sandesh <sandeshbhatta218@gmail.com>
2025-05-30 10:20:58 +0545

    Creating App Layout.

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,27 @@
+import Header from "./Header";
+import CartOveriew from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
+
+function AppLayout() {
+  const navigation = useNavigation();
+
+  const isLoading = navigation.state === "loading";
+
+  return (
+    <div className="grid grid-rows-[auto_1fr_auto] h-screen">
+      {isLoading && <Loader />}
+
+      <Header />
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+
+      <CartOveriew />
+    </div>
+  );
+}
+
+export default AppLayout;
