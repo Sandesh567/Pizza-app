419dbbd619db3d13f9e9ea76b328fbd31bda084a
sandesh <sandeshbhatta218@gmail.com>
2025-06-13 08:47:50 +0545

    using useFetcher hook to fetch data from menu route without loading page.

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,15 +1,28 @@
 // Test ID: IIDSAT
 import OrderItem from "./OrderItem";
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from "../../utilities/helpers";
+import { useEffect } from "react";
 
 function Order() {
   const order = useLoaderData();
+
+  const fetcher = useFetcher();
+
+  useEffect(
+    function () {
+      if (!fetcher.data && fetcher.state === "idle")
+        // by using load and route as menu cause we are loading the data from the menu
+        fetcher.load("/menu");
+    },
+    [fetcher]
+  );
+
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
