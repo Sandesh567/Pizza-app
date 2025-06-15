8471a4fcaf0fceb692fa71e2ead33a91f8e01b74
sandesh <sandeshbhatta218@gmail.com>
2025-06-15 09:26:11 +0545

    Using fetcher.form to interect with route.

--- src/features/order/UpdateOrder.jsx
+++ src/features/order/UpdateOrder.jsx
@@ -1,6 +1,14 @@
+import { useFetcher } from "react-router-dom";
 import Button from "../../ui/Button";
 function UpdateOrder({ order }) {
-  return <Button type="primary">Make a priority</Button>;
+  const fetcher = useFetcher();
+  return (
+    // fetcher.form is a component that allows you to submit forms and interact with
+    // route actions and loaders without causing a full page navigation.
+    <fetcher.Form method="PATCH" className="text-right">
+      <Button type="primary">Make a priority</Button>
+    </fetcher.Form>
+  );
 }
 
 export default UpdateOrder;
